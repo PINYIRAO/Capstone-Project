36e0dc85f3353a67c147c6613541f75da729fe0a
pinyirao <prao2@rrc.ca>
2025-04-05 02:48:50 -0500

    feat: implement the new component for OCR

--- src/api/v1/controllers/uploadController.ts
+++ src/api/v1/controllers/uploadController.ts
@@ -7,15 +7,26 @@
  */
 
 import { Request, Response, NextFunction } from "express";
-import * as courseService from "../services/courseService";
-import type { Course } from "../models/courseModel";
+import type { Course, CourseType } from "../models/courseModel";
 import { errorResponse, successResponse } from "../models/responseModel";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 import tesseract from "tesseract.js";
-import { getErrorMessage } from "../utils/errorUtils";
+import { getErrorCode, getErrorMessage } from "../utils/errorUtils";
 import type { Section } from "../models/courseSectionModel";
 import type { Class } from "../models/courseSectionModel";
 import type { DeliveryType } from "../models/courseSectionModel";
+import {
+  getAllCourses,
+  createCourse,
+  updateCourse,
+} from "../services/courseService";
+import { ServiceError } from "../errors/errors";
+
+// asssumed the overall const for course data
+const program: string = "Application Design and Delivery";
+const userId: string = "admin";
+const term: number = 3;
+const courseType: CourseType = "Required";
 
 /**
  * @description Get the course information and then create or update the course.
@@ -35,9 +46,62 @@ export const uploadCourses = async (
     return;
   } else {
     try {
+      // get the course from the images
+      let sectionsObj: Section[] = [];
       if (Array.isArray(req.files)) {
         for (const file of req.files) {
-          extractSectionData(await ocrEachFile(file));
+          const sectionsObjTemp: Section[] | null = extractSectionData(
+            await ocrEachFile(file)
+          );
+          if (sectionsObjTemp !== null) {
+            sectionsObj = sectionsObj.concat(sectionsObjTemp);
+          }
+        }
+      }
+      // console.log(JSON.stringify(sectionsObj, null, 2));
+      // change the sections info to course structure
+      if (sectionsObj.length !== 0) {
+        const courses: Partial<Course>[] = [];
+        for (const section of sectionsObj) {
+          const regex: RegExp = /^([A-Za-z]+-\d+)/;
+          const match: string[] | null = section.sectionCode.match(regex);
+          if (match !== null) {
+            const courseCode: string = match[1]; //find courseCode
+            const courseName: string = section.sectionName;
+            // create a new course
+            const newCourseInfo: Partial<Course> = {
+              program,
+              term,
+              courseCode,
+              courseName,
+              courseType,
+              userId,
+              courseSections: [section],
+            };
+            if (courses.length == 0) {
+              courses.push(newCourseInfo);
+            }
+            let courseExistFlag: number = 0;
+            for (const c of courses) {
+              if (c.courseCode == courseCode) {
+                // append to the curren course
+                if (c.courseSections) {
+                  c.courseSections.push(section);
+                } else {
+                  c.courseSections = [section];
+                }
+                courseExistFlag = 1;
+              }
+            }
+            if (!courseExistFlag) {
+              courses.push(newCourseInfo);
+            }
+          }
+        }
+        console.log(JSON.stringify(courses, null, 2));
+        // update the course
+        for (const courseObj of courses) {
+          await updateCourseFromImage(courseObj, userId);
         }
       }
 
@@ -65,7 +129,7 @@ async function ocrEachFile(file: Express.Multer.File): Promise<string | null> {
 }
 
 // organize the ocr text to the structured course data
-function extractSectionData(inputText: string | null): Partial<Course> | null {
+function extractSectionData(inputText: string | null): Section[] | null {
   if (inputText == null) {
     return null;
   } else {
@@ -79,12 +143,21 @@ function extractSectionData(inputText: string | null): Partial<Course> | null {
     // ]
     const sectionDeli: RegExp = /(?=COMP-\d+-\w+\d+ Add Section to Schedule)/g;
     const sections: string[] = inputText.split(sectionDeli).filter(Boolean);
+    // console.log(sections);
+    const sectionsObj: Section[] = [];
     for (const section of sections) {
+      let sectionObj: Section;
       let sectionPart0202: string | null;
       let sectionPart0203: string = "";
       let match: string[] | null;
-      let sectionSeats: number;
-      let courseInstructor: string = "";
+      let sectionSeats: number = 999;
+      let sectionInstructor: string = "";
+      // set a default value, it should be determined by the classes type
+      const sectionLectureType: DeliveryType = "Mixed";
+      let sectionCode: string = "NONE";
+      let sectionName: string = "NONE";
+      let sectionStartDate: Date = new Date("1900-01-01");
+      let sectionEndDate: Date = new Date("1900-01-01");
 
       // split the section into two parts
       const sectionDeli01: RegExp =
@@ -105,42 +178,37 @@ function extractSectionData(inputText: string | null): Partial<Course> | null {
         sectionPart01[0].match(regexDeli0101);
       if (SectionPart0101) {
         // example data: COMP-3018-FTE01
-        const sectionCode: string = SectionPart0101[1].trim();
+        sectionCode = SectionPart0101[1].trim();
         // example data: Back-End Development
-        const sectionName: string = SectionPart0101[2]
-          .replace(/\\n/g, " ")
-          .trim();
+        sectionName = SectionPart0101[2].replace(/\\n/g, " ").trim();
         // example data: 2025-01-06T00:00:00.000Z
-        const sectionStartDate: Date = new Date(SectionPart0101[3]); // sectionStartDate
+        sectionStartDate = new Date(SectionPart0101[3]); // sectionStartDate
         // example data: 2025-04-25T00:00:00.000Z
-        const sectionEndDate: Date = new Date(SectionPart0101[4]); // sectionEndDate
-        console.log(sectionCode, sectionName, sectionStartDate, sectionEndDate);
+        sectionEndDate = new Date(SectionPart0101[4]); // sectionEndDate
+        // console.log(sectionCode, sectionName, sectionStartDate, sectionEndDate);
       }
 
       // parse the second part
       // 1. parse the instructor
-      const regexDeli0201: RegExp =
-        /([A-Za-z]+,\s*[A-Za-z])\s*\((Lecture|Online|Lecture,\s*Online)?/;
+      const regexDeli0201: RegExp = /([A-Za-z]+,\s*[A-Za-z])\s*\(([^)]+)\)/;
+      // /([A-Za-z]+,\s*[A-Za-z])\s*\((Lecture|Online|Lecture(,\s*Online)?)/;
       const sectionPart0201: string[] | null =
         sectionPart01[1].match(regexDeli0201);
       if (sectionPart0201) {
         // get the courseInstructor
         // example data: Shabaga, D
-        courseInstructor = sectionPart0201?.[1] || "";
+        sectionInstructor = sectionPart0201?.[1] || "";
         // the left are the second part remove the instructor info
         // example data:
         // 4/35/0 T12:00 PM - 3:00 PM Roblin Centre (Prev. PSC), \nPrincess Building PSCP312\n2025-01-06 - 2025-04-25\nLecture\nW 1:00 PM - 4:00 PM Roblin Centre (Prev. PSC)\n2025-01-06 - 2025-04-25 Online\n
         sectionPart0202 = sectionPart01[1]
-          .replace(
-            /([A-Za-z]+,\s*[A-Za-z])\s*\((Lecture|Online|Lecture,\s*Online)?/g,
-            ""
-          )
+          .replace(/([A-Za-z]+,\s*[A-Za-z])\s*\(([^)]+)\)/g, "")
           .trim();
       } else {
         // just for in case there is no instructor
         sectionPart0202 = sectionPart01[1];
       }
-      console.log(courseInstructor, sectionPart0202);
+      // console.log(sectionInstructor, sectionPart0202);
       // parse the seat data
       const seatsRegex: RegExp = /^(\d+)\/(\d+)\/(\d+)\s*/;
       if (sectionPart0202 !== null) {
@@ -151,7 +219,7 @@ function extractSectionData(inputText: string | null): Partial<Course> | null {
           // example data:
           // T12:00 PM - 3:00 PM Roblin Centre (Prev. PSC), \nPrincess Building PSCP312\n2025-01-06 - 2025-04-25\nLecture\nW 1:00 PM - 4:00 PM Roblin Centre (Prev. PSC)\n2025-01-06 - 2025-04-25 Online\n
           sectionPart0203 = sectionPart0202.replace(seatsRegex, "").trim();
-          console.log(sectionSeats, sectionPart0203);
+          // console.log(sectionSeats, sectionPart0203);
         }
       }
 
@@ -168,9 +236,9 @@ function extractSectionData(inputText: string | null): Partial<Course> | null {
         .map((part) => part.trim())
         .filter((p) => p.length > 0);
       if (parts.length !== 0) {
-        parts.forEach((v) => {
-          console.log(v);
-        });
+        // parts.forEach((v) => {
+        //   console.log(v);
+        // });
         parts.forEach((v) => {
           const classObj: Class[] | null = extractClassData(v);
           if (classObj !== null) {
@@ -178,8 +246,22 @@ function extractSectionData(inputText: string | null): Partial<Course> | null {
           }
         });
       }
-      console.log(classes);
+      // console.log(classes);
+      // eslint-disable-next-line prefer-const
+      sectionObj = {
+        sectionCode,
+        sectionName,
+        sectionInstructor,
+        sectionLectureType,
+        sectionStartDate,
+        sectionEndDate,
+        sectionSeats,
+        sectionSchedules: classes,
+      };
+      sectionsObj.push(sectionObj);
     }
+    // console.log(JSON.stringify(sectionsObj, null, 2));
+    return sectionsObj;
   }
 
   return null;
@@ -189,9 +271,8 @@ function extractClassData(classInfo: string | null): Class[] | null {
   const classesInfo: Class[] = [];
   if (classInfo !== null) {
     // extract the day
-    // const daysRegex: RegExp = /^(\\n)?([A-Za-z/]+)/;
-    const daysRegex: RegExp =
-      /^.*?(\\n)?([A-Za-z/]+)\s+\d{1,2}:\d{2}\s*[AP]M\s*-\s*\d{1,2}:\d{2}\s*[AP]M/;
+    const daysRegex: RegExp = /^(\\n)?([A-Za-z/]+)/;
+    // const daysRegex: RegExp =       /^.*?(\\n)?([A-Za-z/]+)\s+\d{1,2}:\d{2}\s*[AP]M\s*-\s*\d{1,2}:\d{2}\s*[AP]M/;
     const daysMatch: string[] | null = classInfo.match(daysRegex);
     const days: string[] = daysMatch ? daysMatch[2].split("/") : [];
 
@@ -266,3 +347,41 @@ const convertTo24Hour = (time: string): number => {
 
   return newHour * 100 + minute; // convert 3:00 PM into 1500 format
 };
+
+async function updateCourseFromImage(
+  course: Partial<Course>,
+  userId: string
+): Promise<void> {
+  try {
+    let courses: Course[] = await getAllCourses(
+      course.courseCode,
+      course.courseName
+    );
+
+    // filter the course with userId
+    courses = courses.filter((v, _i) => v.userId == userId);
+
+    if (courses.length == 0) {
+      // if there is no record for this course current, then we create a new one
+      await createCourse(course);
+    } else if (courses.length == 1) {
+      // update the current course
+      if (courses[0].id != undefined) {
+        updateCourse(courses[0].id, course);
+      }
+    } else {
+      throw new ServiceError(
+        "The system has more than one records for this course",
+        "DATA_ERROR",
+        HTTP_STATUS.INTERNAL_SERVER_ERROR
+      );
+    }
+  } catch (err) {
+    throw new ServiceError(
+      getErrorMessage(err),
+      getErrorCode(err),
+      HTTP_STATUS.INTERNAL_SERVER_ERROR
+    );
+  }
+  return;
+}
