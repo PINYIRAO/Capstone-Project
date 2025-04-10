f589a79d8cd6ae4c640abdc5d465f472e5fe45bc
pinyirao <prao2@rrc.ca>
2025-04-10 13:46:02 -0500

    backup: backup my work

--- src/api/v1/validations/course/courseCreationValidate.ts
+++ src/api/v1/validations/course/courseCreationValidate.ts
@@ -27,6 +27,20 @@ export const courseCreationSchema = (): ObjectSchema => {
           sectionDeliveryType: Joi.string()
             .required()
             .validate(["Online", "Lecture", "Hybrid"]),
+          sectionStartDate: Joi.date().optional(), //calculated by system
+          sectionEndDate: Joi.date().optional(), //calculated by system
+          sectionSeats: Joi.number().required().min(1),
+          sectionSchedules: Joi.array().items(
+            Joi.object({
+              day: Joi.number().required().validate([0, 1, 2, 3, 4, 5, 6]),
+              deliveryType: Joi.string()
+                .required()
+                .validate(["Online", "Lecture", "Hybrid"]),
+              startTime: Joi.number().min(0).max(24),
+              endTime: Joi.number().min(0).max(24),
+              location: Joi.string().required().min(5),
+            })
+          ),
         })
       ),
   });
